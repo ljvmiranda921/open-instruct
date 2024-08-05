7450a213ff1b15e0df252b703c820f64c3e968ad
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-05 15:04:14 -0700

    Tokenizer updates

--- scripts/split_sharegpt_conversations.py
+++ scripts/split_sharegpt_conversations.py
@@ -104,7 +104,7 @@ def main(args):
         content.extend(json.load(open(file)))
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         args.model_name_or_path,
-        use_fast=False,
+        use_fast=True,
     )
     print(f"Length of content: {len(content)}")
     new_content = split_all(content, args.begin, args.end, tokenizer, args.max_length)
